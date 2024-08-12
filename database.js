dd10ca4a5708f88e752182d2768aa264a95d8c9c
David Torrero <david.torrero@gmail.com>
2024-08-12 12:47:51 +0100

    concernFrogge

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 require('dotenv').config();
+const bcrypt = require('bcrypt');
 
-const dbPath = process.env.DB_PATH || path.join(__dirname, 'albums.db'); // Use DB_PATH from .env
-const db = new sqlite3.Database(dbPath, (err) => {
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'albums.db'); 
+const db = new sqlite3.Database(dbPath, async (err) => {
     if (err) {
         console.error(err.message);
     } else {
@@ -28,6 +29,31 @@ const db = new sqlite3.Database(dbPath, (err) => {
         )`, (err) => {
             if (err) {
                 console.error(err.message);
+            } else {
+                // Check if the users table is empty
+                db.get(`SELECT COUNT(*) AS count FROM users`, async (err, row) => {
+                    if (err) {
+                        console.error(err.message);
+                    } else if (row.count === 0) {
+                        // Insert the initial user
+                        const role = process.env.RADMIN || 'superadmin'; 
+                        const username = 'superadmin';
+                        const password = 'superadmin'; 
+
+                        try {
+                            const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+                            db.run(`INSERT INTO users (username, password, role) VALUES (?, ?, ?)`, [username, hashedPassword, role], (err) => {
+                                if (err) {
+                                    console.error(err.message);
+                                } else {
+                                    console.log('Initial user created: superadmin');
+                                }
+                            });
+                        } catch (hashError) {
+                            console.error('Error hashing password:', hashError.message);
+                        }
+                    }
+                });
             }
         });
 
@@ -62,4 +88,3 @@ const db = new sqlite3.Database(dbPath, (err) => {
 });
 
 module.exports = db;
-
--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1]; 
-
+        
     if (!token) {
         return res.sendStatus(401);
     }
